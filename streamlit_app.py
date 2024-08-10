e3cc2514970d974b2ef34ce1b500c1cc6114302f
Rob <rcu3@njit.edu>
2024-08-09 21:51:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 #Getting Started with Starplex
 import streamlit as st
 import numpy as np
+import altair as alt
+import streamlit as st
+
 
 st.write('Hello, world!')
 age = st.selectbox("Choose your age:", np.arange(18, 66, 1))
+df = pd.DataFrame(np.random.randn(200, 3), columns=['a', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c')
+st.altair_chart(c, width=-1)
